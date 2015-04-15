73ef31a814d8c11cbc3e349e1ee62980f6616071
Dustin Myers <dustint314@gmail.com>
2015-04-15 14:39:08 -0600

    finished, now working on the black diamonds

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-var app = angular.module('chatroom', []);
+var app = angular.module('chatroom', ['ngMaterial']);
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+ 
+}]);
 
 app.config(function($httpProvider){
   $httpProvider.interceptors.push('httpRequestInterceptor');
--- js/parseService.js
+++ js/parseService.js
@@ -6,7 +6,9 @@ app.service('parseService', function($http){
   //On the lines below create a getData method. This method will retrieve data from the parse backend.
   //The url for the get request should be 'https://api.parse.com/1/classes/chat?order=-createdAt'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
- 
+  this.getData = function() {
+    return $http.get('https://api.parse.com/1/classes/chat?order=-createdAt')
+  }
 
 
   //On the line below create the postData method. This method will add data to the parse backend.
@@ -16,7 +18,8 @@ app.service('parseService', function($http){
   //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
   
   //postData method here
+  this.postData = function(inputMessage) {
+    $http.post('https://api.parse.com/1/classes/chat', {text: inputMessage});
+  }
   
-  
-  //getData method here
 });
